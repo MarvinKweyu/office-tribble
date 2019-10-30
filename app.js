492e6d72469b75112bb66bcc99c88a041b6b569f
MarvinKweyu <mkweyu1@gmail.com>
2019-10-30 16:18:46 +0300

    Set up esint and edit server.js

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const knex = require('knex');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const knex = require("knex");
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: ' ',
-        database: 'office-tribble'
-    }
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "MarvinTeam",
+    password: " TeamWork",
+    database: "office-tribble"
+  }
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 module.exports = app;
